46bc620b0fdb20b5fa088c569e6f326e220a52b8
Tahamuhommad <muhommadtaha52@gmail.com>
2024-04-22 15:59:45 +0500

    Initial commit

---
+++ project4/index.ts
@@ -0,0 +1,39 @@
+import inquirer from "inquirer"
+
+const currency :any = {
+     USD:1,//Base Currency
+     EUR:0.91,
+     GBP:0.76,
+     INR:74.57,
+     PKR:280
+};
+
+let userAnswer = await inquirer.prompt(
+  [  {
+        name:'from',
+        message:'Enter from currency',
+        type:'list',
+        choices:['USD','EUR','GBP','INR','PKR']
+    },
+    
+     { name:'to',
+       message:'Enter to currency',
+       type:'list',
+       choices:['USD','EUR','GBP','INR','PKR']
+    
+    } ,
+    {
+        name:"amount",
+        message:"please enter your amount",
+        type:'number'
+    }
+]
+)
+
+let fromAmount = currency[userAnswer.from]
+let toAmount = currency[userAnswer.to]
+let amount = userAnswer.amount
+let baseAmount = amount / fromAmount
+let convertedAmount = baseAmount * toAmount
+
+console.log(convertedAmount);
